96f18b784b55772c477f5615f40d3e5ac249af03
Quaqva <sanek15056@gmail.com>
2024-04-22 16:26:16 +0400

    style: настроено форматирование на сохранение файла, отформатированы все файлы

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
+import routes from './app.routes';
 
-export const appConfig: ApplicationConfig = {
+const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)],
 };
+
+export default appConfig;
