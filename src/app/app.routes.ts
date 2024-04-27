82560daa58efcd2c8ee364b6f7440139351cc118
Quaqva <sanek15056@gmail.com>
2024-04-27 17:03:19 +0400

    feat: настроен роутинг для страницы логина, создана компонента страницы логина

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,12 @@
 import { Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login-page/login-page.component').then(
+        (m) => m.LoginPageComponent,
+      ),
+  },
+];
 export default routes;
