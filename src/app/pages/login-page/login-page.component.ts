d26c29d37986bda683ca2c264a4c1832c48b6827
Quaqva <sanek15056@gmail.com>
2024-04-27 19:03:49 +0400

    feat: сверстана форма авторизации

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { DividerModule } from 'primeng/divider';
+import { ButtonModule } from 'primeng/button';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [
+    InputTextModule,
+    PasswordModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    ReactiveFormsModule,
+    DividerModule,
+    ButtonModule,
+  ],
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.scss'
+  styleUrl: './login-page.component.scss',
 })
 export class LoginPageComponent {
-
+  formGroup = new FormGroup({
+    login: new FormControl(''),
+    password: new FormControl(''),
+  });
 }
