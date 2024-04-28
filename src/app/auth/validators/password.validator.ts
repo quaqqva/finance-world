f12fece88a7ca978d9fd47734fd6f2381334b96f
Quaqva <sanek15056@gmail.com>
2024-04-28 20:57:58 +0400

    fix: исправлена неправильная логика валидации пароля

--- src/app/auth/validators/password.validator.ts
+++ src/app/auth/validators/password.validator.ts
@@ -3,10 +3,10 @@ import { ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
 export default function passwordValidator(): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
     const text = control.value;
-    return {
-      hasLowerCase: !/[a-z]/.test(text),
-      hasUpperCase: !/[A-Z]/.test(text),
-      hasDigit: !/\d/.test(text),
-    };
+    const errors: ValidationErrors = {};
+    if (!/[a-z]/.test(text)) errors['hasLowerCase'] = true;
+    if (!/[A-Z]/.test(text)) errors['hasUpperCase'] = true;
+    if (!/\d/.test(text)) errors['hasDigit'] = true;
+    return errors;
   };
 }
