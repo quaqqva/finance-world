6c68dcdded817667b85189eb48baa107c51f0243
Quaqva <sanek15056@gmail.com>
2024-04-28 19:23:39 +0400

    fix: изменения в сервисе авторизации и добавлена проверка url в интерсепторе

--- src/app/auth/interceptors/auth-response.interceptor.ts
+++ src/app/auth/interceptors/auth-response.interceptor.ts
@@ -5,7 +5,10 @@ import JwtHelper from '../utils/jwt/jwt-helper';
 const authResponseInterceptor: HttpInterceptorFn = (req, next) => {
   return next(req).pipe(
     map((event) => {
-      if (event instanceof HttpResponse) {
+      if (
+        event instanceof HttpResponse &&
+        event.url?.endsWith('pair_settings')
+      ) {
         return event.clone({
           body: {
             accessToken: JwtHelper.GenerateAccessToken(),
--- src/app/auth/services/auth/auth.service.ts
+++ src/app/auth/services/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { AuthToken } from '../../models/auth-token.model';
 
 @Injectable({
@@ -12,8 +13,8 @@ export class AuthService {
     this.httpClient = httpClient;
   }
 
-  async login(login: string, password: string) {
-    const tokens = await this.httpClient.post<AuthToken>('pair_settings', {
+  login(login: string, password: string): Observable<AuthToken> {
+    return this.httpClient.post<AuthToken>('pair_settings', {
       login,
       password,
     });
