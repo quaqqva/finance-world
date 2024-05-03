fec4912eb955e19d062bb6ef4d0c5420a73f7232
Quaqva <sanek15056@gmail.com>
2024-05-03 18:07:43 +0400

    fix: исправлена ошибка CORS

--- src/app/shared/interceptors/url.interceptor.ts
+++ src/app/shared/interceptors/url.interceptor.ts
@@ -3,9 +3,6 @@ import { HttpInterceptorFn } from '@angular/common/http';
 const urlInterceptor: HttpInterceptorFn = (req, next) => {
   const newRequest = req.clone({
     url: `https://api.exmo.me/v1.1/${req.url}`,
-    setHeaders: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
   });
   return next(newRequest);
 };
