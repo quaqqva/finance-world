c7e856f6b2a25142100fb952dcd484601c2451b6
Quaqva <sanek15056@gmail.com>
2024-05-03 16:36:33 +0400

    fix: исправлена ошибка о неучете кириллицы в валидации

--- src/app/pages/auth/components/password-input/password-input.component.ts
+++ src/app/pages/auth/components/password-input/password-input.component.ts
@@ -28,9 +28,11 @@ export class PasswordInputComponent implements OnInit {
 
   private updateRequirements(): void {
     this.requrements = {
-      'Одна строчная буква': this.control.hasError('hasLowerCase'),
-      'Одна заглавная буква': this.control.hasError('hasUpperCase'),
-      'Одна цифра': this.control.hasError('hasDigit'),
+      'Только цифры и латинские буквы':
+        this.control.hasError('hasSpecialSymbols'),
+      'Одна строчная буква': this.control.hasError('noLowerCaseLetter'),
+      'Одна заглавная буква': this.control.hasError('noUpperCaseLetter'),
+      'Одна цифра': this.control.hasError('noDigit'),
       'Минимум 8 символов':
         this.control.hasError('minlength') || this.control.hasError('required'),
       'Максимум 150 символов': this.control.hasError('maxlength'),
--- src/app/pages/auth/validators/password.validator.ts
+++ src/app/pages/auth/validators/password.validator.ts
@@ -4,9 +4,10 @@ export default function passwordValidator(): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
     const text = control.value;
     const errors: ValidationErrors = {};
-    if (!/[a-z]/.test(text)) errors['hasLowerCase'] = true;
-    if (!/[A-Z]/.test(text)) errors['hasUpperCase'] = true;
-    if (!/\d/.test(text)) errors['hasDigit'] = true;
+    if (!/^[a-zA-Z0-9]+$/.test(text)) errors['hasSpecialSymbols'] = true;
+    if (!/[a-zа-я]/.test(text)) errors['noLowerCaseLetter'] = true;
+    if (!/[A-ZА-Я]/.test(text)) errors['noUpperCaseLetter'] = true;
+    if (!/\d/.test(text)) errors['noDigit'] = true;
     return errors;
   };
 }
