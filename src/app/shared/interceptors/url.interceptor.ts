5b40d2d3c3eb77a3acdcd843e321178c9193a819
Quaqva <sanek15056@gmail.com>
2024-05-03 15:08:05 +0400

    fix: исправил content-type в интерсепторе

--- src/app/shared/interceptors/url.interceptor.ts
+++ src/app/shared/interceptors/url.interceptor.ts
@@ -4,7 +4,7 @@ const urlInterceptor: HttpInterceptorFn = (req, next) => {
   const newRequest = req.clone({
     url: `https://api.exmo.me/v1.1/${req.url}`,
     setHeaders: {
-      'Content-Type': '"application/x-www-form-urlencoded"',
+      'Content-Type': 'application/x-www-form-urlencoded',
     },
   });
   return next(newRequest);
