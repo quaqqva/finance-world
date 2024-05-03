9797279b2e4466cca7bcc0d6d73a1f43bb391ffa
Quaqva <sanek15056@gmail.com>
2024-05-03 12:15:26 +0400

    build: настроены флаги ng-cli, удалены все .spec файлы

--- src/app/auth/pipes/format-password-requirement.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FormatPasswordRequirementPipe } from './format-password-requirement.pipe';
-
-describe('FormatPasswordRequirementPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FormatPasswordRequirementPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
