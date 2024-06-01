a72ce1ce7ae9ca609a301a55c1cc3867e0888e68
Quaqva <sanek15056@gmail.com>
2024-06-01 18:15:54 +0400

    feat: добавлен редирект с пустого роута

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,11 @@ import notAuthrorizedGuard from './shared/guards/not-authorized.guard';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
     path: 'home',
     loadChildren: () =>
       import('./pages/home/home.module').then((m) => m.HomeModule),
